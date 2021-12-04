be371f140984919023c51b25dfb140f42ab65a5d
PiterAnnema <piterannema@gmail.com>
2021-12-04 19:26:25 +0100

    Corrected the operator

--- src/day1.py
+++ src/day1.py
@@ -1,10 +1,10 @@
 from itertools import pairwise, starmap
-from operator import __gt__
+from operator import __lt__
 
 
 def main() -> None:
 	with open('data/day1') as f:
-		print(sum(starmap(__gt__, pairwise(map(int, f)))))
+		print(sum(starmap(__lt__, pairwise(map(int, f)))))
 
 
 if __name__ == '__main__':
