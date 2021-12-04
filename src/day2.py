8d7b27ae6bbb70f19e25aeaec6a55c6a14fba356
PiterAnnema <piterannema@gmail.com>
2021-12-04 22:16:04 +0100

    Initial solution day 2

---
+++ src/day2.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+
+@dataclass
+class Position:
+	x: int = 0
+	y: int = 0
+
+	@property
+	def depth(self):
+		return - self.y
+
+	def forward(self, value):
+		self.x += value
+
+	def backward(self, value):
+		self.forward(- value)
+
+	def up(self, value):
+		self.y += value
+
+	def down(self, value):
+		self.up(- value)
+
+
+@dataclass
+class AimedPosition(Position):
+	aim: int = 0
+
+	def forward(self, value):
+		super().forward(value)
+		super().up(value * self.aim)
+
+	def up(self, value):
+		self.aim += value
+
+
+def read_commands():
+	with open('data/day2') as f:
+		for line in f:
+			command, value = line.split()
+			yield command, int(value)
+
+
+def execute_commands(commands, pos):
+	for command, value in commands:
+		getattr(pos, command)(value)
+
+
+def main() -> None:
+	# Part One
+	pos = Position()
+	execute_commands(read_commands(), pos)
+	assert pos.x * pos.depth == 1714680
+
+	# Part Two
+	pos = AimedPosition()
+	execute_commands(read_commands(), pos)
+	assert pos.x * pos.depth == 1963088820
+
+
+if __name__ == '__main__':
+	main()
