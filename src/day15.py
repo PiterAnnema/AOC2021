304e3d146d13eb6b1eed7a9f58726eabeadc0427
PiterAnnema <piterannema@gmail.com>
2021-12-16 14:06:15 +0100

    Switched back from A* to Dijkstra

--- src/day15.py
+++ src/day15.py
@@ -10,39 +10,34 @@ def read_cavern() -> dict[tuple, int]:
     return cavern
 
 
-def get_neighbors(u: tuple):
-    i, j = u
-    return {(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)}
+def get_neighbors(i: int, j: int):
+    return ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1))
 
 
 T = TypeVar('T')
 
 
-def a_star(start: T, target: T, d: Callable, h: Callable, n: Callable) -> dict[T]:
-    '''Pretty straightforward A* algorithm with a priority queue.'''
-    queue = []
-    heapq.heappush(queue, (0, start))
+def dijkstra(start: T, target: T, d: Callable, n: Callable) -> dict[T]:
+    '''Pretty straightforward Dijkstra algorithm with a priority queue.'''
+    queue = [(0, start)]
 
     # We won't resolve the path for this question
     # prev = dict()
 
-    g_scores = dict()
-    g_scores[start] = 0
-
-    f_scores = dict()
-    f_scores[start] = 0
+    dist = {start: 0}
 
     while queue and (u := heapq.heappop(queue)[1]) != target:
         for v in n(u):
-            new_gscore = g_scores[u] + d(u, v)
-            if v not in g_scores or new_gscore < g_scores[v]:
+            if v in dist:
+                continue
+            score = dist[u] + d(u, v)
+            if v not in dist or score < dist[v]:
                 # prev[v] = u
-                g_scores[v] = new_gscore
-                f_scores[v] = new_gscore + h(v)
+                dist[v] = score
 
-                heapq.heappush(queue, (f_scores[v], v))
+                heapq.heappush(queue, (score, v))
 
-    return g_scores
+    return dist
 
 
 def one():
@@ -54,13 +49,10 @@ def one():
     def d(_, v):
         return cavern[v]
 
-    def h(v):
-        return target[0] - v[0] + target[1] - v[1]
-
     def n(u):
-        return filter(lambda v: 0 <= v[0] <= target[0] and 0 <= v[1] <= target[1], get_neighbors(u))
+        return filter(lambda v: 0 <= v[0] <= target[0] and 0 <= v[1] <= target[1], get_neighbors(*u))
 
-    dist = a_star(start, target, d, h, n)
+    dist = dijkstra(start, target, d, n)
     return dist[target]
 
 
@@ -68,8 +60,7 @@ def two():
     '''Part Two'''
     cavern = read_cavern()
     mi, mj = max(cavern)
-    width = mi + 1
-    height = mj + 1
+    width, height = mi + 1, mj + 1
 
     start = (0, 0)
     target = (5 * height - 1, 5 * width - 1)
@@ -78,13 +69,11 @@ def two():
         i, j = v
         return (cavern[i % height, j % width] + i // height + j // width - 1) % 9 + 1
 
-    def h(v):
-        return target[0] - v[0] + target[1] - v[1]
-
     def n(u):
-        return filter(lambda v: 0 <= v[0] <= target[0] and 0 <= v[1] <= target[1], get_neighbors(u))
+        i, j = u
+        return filter(lambda v: 0 <= i <= target[0] and 0 <= j <= target[1], get_neighbors(i, j))
 
-    dist = a_star(start, target, d, h, n)
+    dist = dijkstra(start, target, d, n)
     return dist[target]
 
 
